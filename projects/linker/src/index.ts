8af70ebf2c531b6ba3b16a5c7e04d2668c006552
NaN-Simon <goosaimon@gmail.com>
2023-07-02 16:39:06 +0300

    refactor: linker, eng-cards

--- projects/linker/src/index.ts
+++ projects/linker/src/index.ts
@@ -1,47 +1,43 @@
-import'./styles/styles.scss'
+import './styles/styles.scss';
 
 /* Code */
 
-class Linker{
-  container: HTMLElement
-  button: HTMLElement
-  inputArea: HTMLInputElement
-  outputArea: HTMLInputElement
-  
+class Linker {
+  container: HTMLElement;
+  button: HTMLElement;
+  inputArea: HTMLInputElement;
+  outputArea: HTMLInputElement;
 
-  constructor(){
-    this.container=document.body
-    this.inputArea = document.querySelector('[name="in"]') as HTMLInputElement
-    this.outputArea = document.querySelector('[name="out"]') as HTMLInputElement
-    this.button = document.querySelector('[button="lets"]') as HTMLButtonElement
+  constructor() {
+    this.container = document.body;
+    this.inputArea = document.querySelector('[name="in"]') as HTMLInputElement;
+    this.outputArea = document.querySelector(
+      '[name="out"]'
+    ) as HTMLInputElement;
+    this.button = document.querySelector(
+      '[button="lets"]'
+    ) as HTMLButtonElement;
   }
-  
+
   translate() {
-    const mySet = new Set()
-    const inValue = this.inputArea.value
-    const temp = inValue.split('\n')
-    temp.forEach(item=>mySet.add(item))
-    const temp1: string[] = (Array.from(mySet)) as string[]
-    const temp2 = temp1.sort((one, two) => (one < two ? -1 : 1));
-    const temp3: string[] = []
-    temp2.forEach((el)=>{
-      // temp3.push(el)
-      if(((el.indexOf('http'))==0)){
-        temp3.push(`<p><a href="${el}">${el}</a></p>`)
-      }
-
-    })
-    this.outputArea.value = temp3.join('\n')
-
+    const mySet = new Set();
+    const inputValueArray = this.inputArea.value.split('\n');
+    inputValueArray.forEach((item) => mySet.add(item));
+    const arrayOfString: string[] = Array.from(mySet) as string[];
+    const sortedArray = arrayOfString.sort((one, two) => (one < two ? -1 : 1));
+    const linkerResult: string[] = [];
+    sortedArray.forEach((el) => {
+      if (el !== "") { linkerResult.push(`<p><a class="link" href="${el}">${el}</a></p>`) }
+    });
+    this.outputArea.value = linkerResult.join('\n');
   }
 
-  render(){
-    this.button.addEventListener('click',()=>{
-      this.translate()
-    })
-
+  render() {
+    this.button.addEventListener('click', () => {
+      this.translate();
+    });
   }
 }
 
-const linker = new Linker()
-linker.render()
+const linker = new Linker();
+linker.render();
